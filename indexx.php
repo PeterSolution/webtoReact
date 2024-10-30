104a633b5c8966351b10848e6d566f35e1078cdd
PeterSolution <141070789+PeterSolution@users.noreply.github.com>
2024-10-30 06:51:57 +0100

    Add files via upload

---
+++ indexx.php
@@ -0,0 +1,258 @@
+<!doctype html>
+<html>
+
+<head>
+<link rel="stylesheet" href="index.css">
+<title>Moja strona</title>
+<meta charset="UTF-8">
+</head>
+
+<body>
+    
+<div id="header1">
+        <h1>Piotr Wolff</h1>
+      </div>
+	  <div id="header22">
+      <div id="header2">
+        Wiedza
+        Doswiadczenie
+        Osiagniecia<br/><center>
+        Umiejetnosci
+        Cel</center>
+      </div>
+      <div id="header2">
+        <center><h2 id="center50px">Kontakt</h2><br/>
+        <div style="margin-left: 120px;">
+		E-mail: PiotrWolff@op.pl
+        <br />
+		Telefon: 111222333<br/>
+		</div>
+		</center>
+      </div>
+
+      <div id="newsection">
+        <center><h1 >Wiedza</h1></center>
+      </div>
+      <div id="bodyfull2">
+        <div id="fullsection">
+		<div id="control1">
+			<center><b>Programowanie:</b></center>
+			<b>C#</b><br/>
+			Java<br/>
+			SQL<br/>
+			PHP<br/>
+			C++<br/>
+		</div>
+		<div id="control2">
+			<center><b>Sieci</b><br/>
+			Przygotowanie do kursu CCNA
+			
+			</center>
+		</div>
+		<div id="control1">
+			<center><b >Frameworki</b><br/>
+			Entity Framework (C#)<br/>
+			Mapper (C#)<br/>
+			Numpy (python)<br/>
+			pyautogui (python)<br/>
+			flask (python)<br/>
+			</center>
+			
+		</div>
+	</div>
+        
+	</div>
+
+
+	</div>
+      <div id="bodyfull2">
+        <h1>C#</h1>
+        <h2>Moja droga z C#</h2><br />
+        <h2>C# to mój glólny język programowania gdzie pisałem programy w sieci lan, aplikacje serwerowe typu Rest API oraz aplikacje klienckie
+        </h2><br></br>
+        <h2>jaki to jezyk</h2><br />
+        <h2>
+          C# to nowoczesny, wieloparadygmatowy język programowania, który łączy w sobie cechy programowania obiektowego i funkcjonalnego. Charakteryzuje się silnym typowaniem i składnią zbliżoną do C++ i Javy. C# oferuje zaawansowane funkcje, takie jak:</h2>
+        <ul>
+          <li>Garbage collection do automatycznego zarządzania pamięcią</li>
+          <li>Właściwości, które umożliwiają łatwe implementowanie getterów i setterów</li>
+          <li>LINQ (Language Integrated Query) do efektywnego przetwarzania danych</li>
+          <li>Async/await do asynchronicznego programowania</li>
+          <li>Rozszerzalne typy wartości (structs) i referencyjne (classes)</li>
+        </ul>
+        <h2>C# ewoluuje wraz z każdym nowym wydaniem, wprowadzając innowacyjne funkcje jak pattern matching czy nullable reference types.
+        </h2><br />
+
+      </div>
+      <div id="bodyfull">
+
+        <h1>Java</h1>
+        <h2>Moja droga nauki z Java</h2>
+        <h2>Java to język który jest dla mnie najnowsszy i poznałem go rok temu. W tym języku tworzyłem aplikacje zarządzania wątkami i aplikacje klienckie</h2>
+        <br />
+        <h2>Jaki to jezyk</h2><br />
+        <p>Java to obiektowy język programowania, znany z swojej filozofii "write once, run anywhere". Kluczowe cechy Javy to:</p>
+        <ul>
+          <li>Niezależność od platformy dzięki Java Virtual Machine (JVM)</li>
+          <li>Automatyczne zarządzanie pamięcią poprzez garbage collection</li>
+          <li>Silne typowanie, co pomaga w wykrywaniu błędów na etapie kompilacji</li>
+          <li>Bogata biblioteka standardowa (Java Class Library)</li>
+          <li>Wsparcie dla programowania współbieżnego</li>
+          <li>Refleksja, umożliwiająca dynamiczne tworzenie i modyfikowanie obiektów w czasie wykonania</li>
+        </ul>
+        <p>Java słynie z swojej stabilności i skalowalności, co czyni ją popularnym wyborem w środowiskach korporacyjnych.</p>
+
+
+      </div>
+      <div id="bodyfull">
+        <h1>SQL</h1><br />
+        <h2>Moja droga z SQL</h2><br />
+        <h2>Wiem jak zarządzać bazą danych przy pomocy czystego SQL oraz umiem zastosować to w kodzie zarówno z zasadom "Code first" jak i "Database first". W aplikacjach serwerowych w C# pisałem połączenia używając code first przy użyciu entity framework</h2><br />
+          <p>SQL (Structured Query Language) to deklaratywny język programowania zaprojektowany do zarządzania danymi w relacyjnych bazach danych. Jego główne cechy to:</p>
+          <ul>
+            <li>Standardizacja – SQL jest standardem ANSI/ISO, choć różne systemy baz danych mogą mieć swoje dialekty</li>
+            <li>Deklaratywność – użytkownik określa, jakie dane chce uzyskać, a nie jak je uzyskać</li>
+            <li>Podzielony na podgrupy: DDL (Data Definition Language), DML (Data Manipulation Language), DCL (Data Control Language)</li>
+            <li>Obsługa transakcji zapewniająca integralność danych</li>
+            <li>Możliwość tworzenia złożonych zapytań poprzez łączenie tabel i używanie funkcji agregujących</li>
+          </ul>
+          <p>SQL umożliwia efektywne zarządzanie dużymi ilościami danych strukturalnych.</p>
+
+      </div>
+      <div id="bodyfull">
+        <h1>PHP</h1><br />
+        <h2>Moja droga z PHP</h2><br />
+        <h2>Jest to język którego głównie używałem przy egzaminie zawodowym. Używając go uzyskałem tytół technika.</h2><br />
+        <p>PHP (PHP: Hypertext Preprocessor) to interpretowany język skryptowy, oryginalnie zaprojektowany do tworzenia dynamicznych stron internetowych. Charakterystyczne cechy PHP to:</p>
+        <ul>
+          <li>Łatwość integracji z HTML</li>
+          <li>Duża elastyczność w składni, co ułatwia początkującym naukę</li>
+          <li>Bogaty zestaw funkcji do pracy z bazami danych, plikami i protokołami internetowymi</li>
+          <li>Wsparcie dla programowania obiektowego od wersji 5</li>
+          <li>Wbudowane mechanizmy sesji i ciasteczek</li>
+          <li>Duża społeczność i bogaty ekosystem bibliotek (Composer)</li>
+        </ul>
+        <p>PHP ewoluował na przestrzeni lat, wprowadzając nowe funkcje jak typy skalarne czy anonimowe klasy.</p>
+      </div>
+
+      <div id="bodyfull">
+        <h1>C++</h1><br />
+        <h2>Moja droga z C++</h2><br />
+        <h2>Nigdy nie miałem problemu z naukami nowych języków. Był to język od jakiego zacząłem już w pierwszej klasie gimnazjum. Ucząc się tego języka zrozumiałem strukturę kodu oraz jak pisać podstawowy kod</h2><br />
+          <p>C++ to wieloparadygmatowy język programowania, rozszerzający możliwości języka C. Główne cechy C++ to:</p>
+          <ul>
+            <li>Wsparcie dla programowania obiektowego, generycznego i funkcjonalnego</li>
+            <li>Bezpośredni dostęp do pamięci i możliwość niskopoziomowych operacji</li>
+            <li>Wydajność i kontrola nad zasobami systemu</li>
+            <li>Szablony umożliwiające metaprogramowanie</li>
+            <li>Przeciążanie operatorów</li>
+            <li>Obsługa wyjątków</li>
+            <li>Standard Template Library (STL) oferująca gotowe struktury danych i algorytmy</li>
+          </ul>
+          <p>C++ jest stale rozwijany, z nowymi standardami wprowadzającymi funkcje jak auto, lambda wyrażenia czy smart pointery.</p>
+      </div>
+
+      <div id="bodyfull">
+        <h1>Kurs CCNA</h1><br />
+        <h2>Moja droga z CCNA</h2><br />
+        <h2>Przygotowuję się do kursu z sieci komputerowych o nazwie CCNA. Uczę się o przesyle danych w sieci oraz o strukturze i funkcjonowaniu jej.</h2><br />
+          <p>CCNA (Cisco Certified Network Associate) to certyfikat potwierdzający umiejętności w zakresie sieci komputerowych. Kurs CCNA obejmuje:</p>
+          <ul>
+            <li>Podstawy sieci i modele OSI/TCP-IP</li>
+            <li>Konfigurację urządzeń sieciowych Cisco</li>
+            <li>Protokoły routingu i przełączania</li>
+            <li>Bezpieczeństwo sieci</li>
+            <li>Podstawy sieci WAN i chmury obliczeniowej</li>
+            <li>Automatyzację i programowalność sieci</li>
+          </ul>
+          <p>Certyfikat CCNA jest ceniony w branży IT i otwiera drogę do kariery w obszarze sieci komputerowych.</p>
+      </div>
+
+      <div id="bodyfull">
+        <h1>Mapper (C#)</h1><br />
+        <h2>Moja droga z Mapper</h2><br />
+        <h2>Poznałem to narzędzie rzutując obiekty na inne. Jest to rzecz dzięki której usprawniła moją prace. Biblioteka ta uświadomiła mi jak ważne jest "clean code"</h2><br />
+          <p>Mapper to narzędzie w C# służące do automatycznego mapowania obiektów. Jego główne cechy to:</p>
+          <ul>
+            <li>Automatyczne mapowanie właściwości między obiektami różnych typów</li>
+            <li>Konfigurowalność mapowań dla niestandardowych przypadków</li>
+            <li>Wsparcie dla mapowania kolekcji i typów zagnieżdżonych</li>
+            <li>Możliwość tworzenia map w czasie kompilacji, co poprawia wydajność</li>
+            <li>Integracja z popularnymi frameworkami .NET</li>
+          </ul>
+          <p>Używanie Mappera przyczynia się do tworzenia czystszego i łatwiejszego w utrzymaniu kodu.</p>
+      </div>
+
+      <div id="bodyfull">
+        <h1>Numpy (python)</h1><br />
+        <h2>Moja droga z Numpy</h2><br />
+        <h2>Jest to biblioteka stanowiąca wydaje mi się podstawy do nauki pythona. Ucząc się języków próbuję nauczyć się też bibliotek i struktór.</h2><br />
+          <p>NumPy to fundamentalna biblioteka do obliczeń naukowych w Pythonie. Główne cechy NumPy to:</p>
+          <ul>
+            <li>Wydajne struktury danych do przechowywania dużych tablic i macierzy</li>
+            <li>Szeroka gama funkcji matematycznych do operacji na tablicach</li>
+            <li>Narzędzia do integracji kodu C/C++ i Fortran</li>
+            <li>Zaawansowane możliwości w zakresie algebry liniowej, transformacji Fouriera i liczb losowych</li>
+            <li>Wsparcie dla obliczeń równoległych</li>
+          </ul>
+          <p>NumPy stanowi podstawę wielu innych bibliotek do analizy danych i uczenia maszynowego w Pythonie.</p>
+      </div>
+
+      <div id="bodyfull">
+        <h1>pyautogui (python)</h1><br />
+        <h2>Moja droga z pyautogui</h2><br />
+        <h2>Biblioteka pyautogui wspaniale działała z różnymi botami jakie pisałem, używając jej zrozumiałem jak komputer znajduje obraz, jak na obraz w programowaniu wpływa rozdzielczość oraz jak znaleźć dany punkt na ekranie.</h2><br />
+          <p>PyAutoGUI to biblioteka Pythona do automatyzacji interakcji z interfejsem graficznym. Główne funkcje PyAutoGUI to:</p>
+          <ul>
+            <li>Kontrola myszy i klawiatury</li>
+            <li>Wykonywanie zrzutów ekranu</li>
+            <li>Rozpoznawanie obrazów na ekranie</li>
+            <li>Symulacja kliknięć i wpisywania tekstu</li>
+            <li>Przenośność między systemami Windows, macOS i Linux</li>
+          </ul>
+          <p>PyAutoGUI jest szczególnie przydatne do tworzenia botów, automatyzacji testów GUI i automatyzacji zadań powtarzalnych.</p>
+      </div>
+
+      <div id="bodyfull">
+        <h1>flask (python)</h1><br />
+        <h2>Moja droga z Flask</h2><br />
+        <h2>Flask to lekki framework webowy dla Pythona, który poznałem podczas rozwijania moich umiejętności w tworzeniu aplikacji internetowych.</h2><br />
+        <p>Flask to mikro-framework webowy napisany w Pythonie. Jego główne cechy to:</p>
+        <ul>
+          <li>Prostota i minimalizm, pozwalające na szybkie tworzenie aplikacji</li>
+          <li>Wbudowany serwer deweloperski i debugger</li>
+          <li>Zintegrowane wsparcie dla testów jednostkowych</li>
+          <li>Obsługa rozszerzeń do dodawania funkcjonalności</li>
+          <li>Łatwa integracja z bazami danych i innymi bibliotekami Pythona</li>
+        </ul>
+        <p>Flask jest często wybierany do tworzenia małych i średnich aplikacji webowych oraz API.</p>
+      </div>
+
+      <div id="bodyfullforscript">
+        <h1>Javascript</h1><br />
+        <h2>Moja droga z Javascript</h2><br />
+        <h2>Pierwszy raz poznałem javascript kiedy uczęszczałem do technikum i przygotowywałem się na egzamin zawodowy</h2><br />
+        <p>Pokazanie działania javascripta</p><br/>
+        <br/>
+		
+		<input type="color" id="colorPicker">
+		<script>
+
+		document.getElementById('colorPicker').addEventListener('input', function() {
+			
+			const selectedColor = this.value;
+			
+			document.getElementById("bodyfullforscript").style.backgroundColor = selectedColor;
+		});
+
+    </script>
+		
+      </div>
+
+
+      <div id="fotter">
+      </div>
+
+</body>
+
+</html>
